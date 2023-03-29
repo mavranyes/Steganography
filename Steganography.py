d7109c1b61b55015325202992c474b6233d36d40
mavranyes <mavranyes@comcast.net>
2023-03-29 13:06:11 -0400

    Improved adjustable len & added cleaning function

--- Steganography.py
+++ Steganography.py
@@ -1,12 +1,15 @@
 #Created by Micah Vranyes
 #Date 3/28/2023
-#Version 1.0
+#Version 1.1
 
 import sys
+import random
+import time
 
-def extractBinary(fileName, msgLen, offset):
+def extractBinary(fileName, msgLen):
+    global offset
     with open(fileName, "rb") as imageFile:
-        imageFile.read(OFF_SET + offset)
+        imageFile.read(offset)
         binaryMsg = ""
         for x in range(msgLen):
             byte = bytearray(imageFile.read(1))
@@ -14,64 +17,81 @@ def extractBinary(fileName, msgLen, offset):
                 binaryMsg += "1"
             else:
                 binaryMsg += "0"
+    offset += msgLen
     return binaryMsg
 
+def replaceBinary(imageFileName, binaryMsg):
+    with open(imageFileName, "r+b") as imageFile:
+            imageFile.seek(offset)
+            bytePlace = offset
+            for diget in binaryMsg:  
+                byte = bytearray(imageFile.read(1))
+                #Modify LSD
+                if (byte[0] % 2 == 1):
+                    byte[0] -= 1
+                if (diget == "1"):
+                    byte[0] += 1
+                #Replaces byte in image
+                imageFile.seek(bytePlace)
+                imageFile.write(byte)
+                bytePlace += 1
+                imageFile.seek(bytePlace)
+
 try:
+    global offset 
+    offset = 100
     secret = ""
-    OFF_SET = 100
+    msgLenInd = 16
+    maxMsgCharSize = pow(2, msgLenInd) / 8
     operator = sys.argv[1]
     if operator == "-e":#If encrypt
         #Recieve user message input
         if len(sys.argv) >= 5:
             textFileName = sys.argv[4]
             testFile = open(textFileName, "r")
-            secret = testFile.read()
+            asciiMsg = testFile.read()
         else:
-            secret = input("Enter the secret message: ")
+            asciiMsg = input("Enter the secret message: ")
         #Convert ascii to binary
-        binaryForm = "".join(format(ord(x), "08b") for x in secret)
-        msgLength = len(binaryForm)
-        print(msgLength)
-        binMsgLength = format(msgLength, '0{}b'.format(8))
-        print(binMsgLength)
-        binaryForm = binMsgLength + binaryForm
-        #Open image and replace each LSB
+        binMsg = "".join(format(ord(x), "08b") for x in asciiMsg)
+        #Find message length
+        if len(binMsg) > maxMsgCharSize:
+            print("Error: Message length cannot exceed %d characters!" % (int(maxMsgCharSize)))
+            sys.exit()
+        binMsg = format(len(binMsg), '0{}b'.format(msgLenInd)) + binMsg
+        #Open image and replace each LSD
         imageFileName = sys.argv[2]
-        imageFile = open(imageFileName, "r+b")
-        imageFile.seek(OFF_SET)
-        bytePlace = OFF_SET
-        for diget in binaryForm:  
-            byte = bytearray(imageFile.read(1))
-            #Modify LSD
-            byte[0] -= 1
-            if (byte[0] % 2 == 1):
-                byte[0] -= 1
-            if (diget == "1"):
-                byte[0] += 1
-            #Replaces byte in image
-            imageFile.seek(bytePlace)
-            imageFile.write(byte)
-            bytePlace += 1
-            imageFile.seek(bytePlace)
-
+        replaceBinary(imageFileName, binMsg)
+        
     elif operator == "-d":#If decrpyt
-        # #Find message length
+        #Find message length
         imageFileName = sys.argv[2]
-        msgLenStr = extractBinary(imageFileName, 8, 0)        
-        msglen = int(format(int(msgLenStr, 2), "d"))
-        print(msglen)
+        msgLenStr = extractBinary(imageFileName, msgLenInd)   
+        decMsgLen = int(format(int(msgLenStr, 2), "d"))
         #Open image and grab each LSB
-        binaryMsg = extractBinary(imageFileName, msglen*8, 8)
+        binaryMsg = extractBinary(imageFileName, decMsgLen)
         #Binary string to ASCII
-        for x in range(int(msglen/8)):
+        for x in range(int(decMsgLen/8)):
             upBnd = int((x * 8) + 8)
             lwBnd = int(x * 8)
             secret = secret + format(int(format(int(binaryMsg[lwBnd:upBnd], 2), "d")), "c")
-        #Insert into file
-        textFileName = sys.argv[3]
-        with open(textFileName, "w") as textFile:
-            textFile.write(secret)
+        #Insert into file or stdout
+        if len(sys.argv) >= 4:
+            textFileName = sys.argv[3]
+            with open(textFileName, "w") as textFile:
+                textFile.write(secret)
+        else:
+            print(secret)
 
+    elif operator == "-c":#If cleaning
+        imageFileName = sys.argv[2]
+        #Create random binary string
+        random.seed(round(time.time() * 1000))
+        asciiMsg = "".join(chr(round(random.random()*100)) for x in range(int(maxMsgCharSize)))
+        binMsg = "".join(format(ord(x), "08b") for x in asciiMsg)
+        #Open image and replace each LSD
+        replaceBinary(imageFileName, binMsg)
+        
     print("Process Finished")
 except OSError:
     print("Error: ASCII input file does not exist")
@@ -79,3 +99,4 @@ except IndexError:
     print("Error: Too few arguments")
     print("Format for encryption: -e <original file image name> <modified image name> [input ASCII text file name]")
     print("Format for decryption: -d <modified image name> [output ASCII text file name]")
+    print("Format for cleaning: -d <modified image name>")
